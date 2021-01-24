e52c45adc07ba2abbb3f828ea814ce20054aac73
V Guardado <auricia.guardado@gmail.com>
2021-01-24 01:41:58 -0800

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Eclipse');
+INSERT INTO burgers (name) VALUES ('New Moon');
+INSERT INTO burgers (name, sleepy) VALUES ('Rodeo', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Crescent', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Buffalo', true);
+INSERT INTO burgers (name) VALUES ('Breakfast');
