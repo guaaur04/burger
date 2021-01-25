4823a1b0602b76cfb7b5d5a4e30d45b691973e75
V Guardado <auricia.guardado@gmail.com>
2021-01-24 18:35:51 -0800

    sql helpers

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,48 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+
+// Helper function for SQL syntax
+
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+
+// Helper function to convert object key/value pairs to SQL syntax
+
+function objToSql(ob) {
+    var arr = [];
+  
+    // loop through keys and push key/value as a string int arr
+
+    for (var key in ob) {
+      var value = ob[key];
+
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+
 // Create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database
 
 //selectAll()
 
+
 // insertOne()
+
 
 // updateOne()
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,4 +8,3 @@
 
 // Create the router for the app, and export the router at the end of your file.
 
-// View setup
