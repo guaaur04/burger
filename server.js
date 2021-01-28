5f01a108bea80f5b0204f2a87fb225b9f1a3c832
V Guardado <auricia.guardado@gmail.com>
2021-01-27 23:38:31 -0800

    changes

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-const db = requiere("./models")
+const db = require("./models");
+
+//Require our models for synching
+
+// const db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -16,16 +20,22 @@ app.use(express.json());
 // Set Handlebars.
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+   defaultLayout: "main"
+   }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
+
 const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
+db.sequelize.sync().then(function() {
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
+});
+
 });
